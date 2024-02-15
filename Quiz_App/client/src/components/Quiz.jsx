e0d2ab32d2259bb1974653e03e543b28c2330fa3
Himanshu <hv65616@gmail.com>
2024-02-15 21:38:58 +0530

    quiz component dev

--- Quiz_App/client/src/components/Quiz.jsx
+++ Quiz_App/client/src/components/Quiz.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import Question from "./Question";
 function Quiz() {
-  return <div>Quiz</div>;
+  const prevQuestion = () => {
+    console.log("Prev Buttton");
+  };
+  const nextQuestion = () => {
+    console.log("Next button");
+  };
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+      <Question></Question>
+      <div className="grid">
+        <button className="btn prev" onClick={prevQuestion}>
+          Prev
+        </button>
+        <button className="btn next" onClick={nextQuestion}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
