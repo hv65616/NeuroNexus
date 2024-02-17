208f35b8b30421fce2c7083ffb482be32447505b
Himanshu <hv65616@gmail.com>
2024-02-17 19:28:45 +0530

    result table component

---
+++ Quiz_App/client/src/components/ResultTable.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function ResultTable() {
+  return (
+    <div>
+      <table>
+        <thead className="table-header">
+          <tr className="table-row">
+            <td>Name</td>
+            <td>Attempt</td>
+            <td>Earn Points</td>
+            <td>Result</td>
+          </tr>
+        </thead>
+        <tbody>
+            <tr className='table-body'>
+                <td>DT</td>
+                <td>DT</td>
+                <td>DT</td>
+                <td>DT</td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ResultTable
