a7b5febc4a37e992774f886b9565003009d233a2
Himanshu <hv65616@gmail.com>
2024-02-22 23:42:28 +0530

    result array and option highlight

--- Quiz_App/client/src/components/Quiz.jsx
+++ Quiz_App/client/src/components/Quiz.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Question from "./Question";
 import { moveNextQuestion } from "../hooks/FetchQuestions";
 import { movePrevQuestion } from "../hooks/FetchQuestions";
 import { useSelector, useDispatch } from "react-redux";
+import { pushAnswer } from "../hooks/setResult";
 function Quiz() {
+  const [check, setChecked] = useState(undefined);
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
   useEffect(() => {
     // console.log(state.questions.trace);
     // console.log(state.questions);
     // console.log(state.questions.queue.length);
+    console.log(state);
   });
   const prevQuestion = () => {
     // console.log("Prev Buttton");
@@ -19,14 +22,19 @@ function Quiz() {
   };
   const nextQuestion = () => {
     // console.log("Next button");
-    if (state.questions.trace < state.questions.queue.length-1) {
+    if (state.questions.trace < state.questions.queue.length - 1) {
       dispatch(moveNextQuestion());
+      dispatch(pushAnswer(check));
     }
+  };
+  const onchecked = (checked) => {
+    // console.log(checked);
+    setChecked(checked);
   };
   return (
     <div className="container">
       <h1 className="title text-light">Quiz Application</h1>
-      <Question></Question>
+      <Question onchecked={onchecked}></Question>
       <div className="grid">
         <button className="btn prev" onClick={prevQuestion}>
           Prev
