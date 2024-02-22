507f0f2eeded8bbdd5ac984be07e723d98275758
Himanshu <hv65616@gmail.com>
2024-02-22 16:00:45 +0530

    make the next and prev button functional

--- Quiz_App/client/src/components/Quiz.jsx
+++ Quiz_App/client/src/components/Quiz.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
 import Question from "./Question";
-import { useSelector } from "react-redux";
+import { moveNextQuestion } from "../hooks/FetchQuestions";
+import { movePrevQuestion } from "../hooks/FetchQuestions";
+import { useSelector, useDispatch } from "react-redux";
 function Quiz() {
   const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   useEffect(() => {
-    // console.log(state);
+    // console.log(state.questions.trace);
+    // console.log(state.questions);
+    // console.log(state.questions.queue.length);
   });
   const prevQuestion = () => {
     // console.log("Prev Buttton");
+    if (state.questions.trace > 0) {
+      dispatch(movePrevQuestion());
+    }
   };
   const nextQuestion = () => {
     // console.log("Next button");
+    if (state.questions.trace < state.questions.queue.length-1) {
+      dispatch(moveNextQuestion());
+    }
   };
   return (
     <div className="container">
