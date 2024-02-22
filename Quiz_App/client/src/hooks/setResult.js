88e034f67a7af95cbe48d503182724586b659dfe
Himanshu <hv65616@gmail.com>
2024-02-22 23:37:23 +0530

    created hook to set option

---
+++ Quiz_App/client/src/hooks/setResult.js
@@ -0,0 +1,8 @@
+import * as Action from "../redux/result_reducer";
+export const pushAnswer = (result) => async (dispatch) => {
+  try {
+    await dispatch(Action.pushResultAction(result));
+  } catch (error) {
+    console.log(error);
+  }
+};
