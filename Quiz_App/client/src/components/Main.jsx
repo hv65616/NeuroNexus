191bc9e85374ab2e9e07b21a3e21e98aab9d38b8
Himanshu <hv65616@gmail.com>
2024-02-26 19:01:56 +0530

    setting the userid

--- Quiz_App/client/src/components/Main.jsx
+++ Quiz_App/client/src/components/Main.jsx
@@ -1,8 +1,16 @@
 import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Main.css";
+import { useDispatch } from "react-redux";
+import { setuserid } from "../redux/result_reducer";
 function Main() {
   const inputref = useRef(null);
+  const dispatch = useDispatch();
+  const startQuiz = () => {
+    if (inputref.current?.value) {
+      dispatch(setuserid(inputref.current?.value));
+    }
+  };
   return (
     <div className="container">
       <h1 className="title text-light">Quiz Application</h1>
@@ -14,10 +22,15 @@ function Main() {
         <li>The result will be declared after successful completion of quiz</li>
       </ol>
       <form id="form">
-        <input ref={inputref} type="text" placeholder="Enter your name" className="userid"/>
+        <input
+          ref={inputref}
+          type="text"
+          placeholder="Enter your name"
+          className="userid"
+        />
       </form>
       <div className="start">
-        <Link className="btn" to={"quiz"}>
+        <Link className="btn" to={"quiz"} onClick={startQuiz}>
           Start Quiz
         </Link>
       </div>
