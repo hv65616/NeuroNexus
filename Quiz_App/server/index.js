0c902937faf8a485f85e3d66197dc2342793f7b1
Himanshu <hv65616@gmail.com>
2024-02-27 10:47:42 +0530

    including routes.js

--- Quiz_App/server/index.js
+++ Quiz_App/server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const router = require("../server/router/routes");
 const dotenv = require("dotenv");
-dotenv.config({path: "./config.env"})
+dotenv.config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT || 8081;
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", router);
 
 app.get("/", (req, res) => {
   try {
