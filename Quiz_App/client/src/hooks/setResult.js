b8ea2c694bc7195c13371b4b07e3ea8edb8a03fa
Himanshu <hv65616@gmail.com>
2024-02-29 01:00:23 +0530

    set result to database

--- Quiz_App/client/src/hooks/setResult.js
+++ Quiz_App/client/src/hooks/setResult.js
@@ -1,4 +1,5 @@
 import * as Action from "../redux/result_reducer";
+import { postServerData } from "../helper/helper";
 export const pushAnswer = (result) => async (dispatch) => {
   try {
     await dispatch(Action.pushResultAction(result));
@@ -13,4 +14,20 @@ export const updateResult = (index) => async (dispatch) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+export const usePublishResult = (resultData) => {
+  const { result, username } = resultData;
+  (async () => {
+    try {
+      if (result.length<=0 && !username) throw new Error("Couldn't get Result");
+      await postServerData(
+        `${process.env.REACT_APP_SERVER_HOSTNAME}/api/result`,
+        resultData,
+        (data) => data
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  })();
 };
