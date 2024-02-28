b3fa7f1fa67714606dedcfac147612ceb4a2f1ac
Himanshu <hv65616@gmail.com>
2024-02-29 01:15:54 +0530

    result table working

--- Quiz_App/client/src/components/ResultTable.jsx
+++ Quiz_App/client/src/components/ResultTable.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { getServerData } from "../helper/helper";
 function ResultTable() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getServerData(
+      `${process.env.REACT_APP_SERVER_HOSTNAME}/api/result`,
+      (res) => setData(res)
+    );
+  });
   return (
     <div>
       <table>
@@ -13,16 +20,19 @@ function ResultTable() {
           </tr>
         </thead>
         <tbody>
-            <tr className='table-body'>
-                <td>DT</td>
-                <td>DT</td>
-                <td>DT</td>
-                <td>DT</td>
+          {!data ?? <div>No Data Found</div>}
+          {data.map((v, i) => (
+            <tr className="table-body" key={i}>
+              <td>{v?.username || ""}</td>
+              <td>{v?.attempts || 0}</td>
+              <td>{v?.points || 0}</td>
+              <td>{v?.achived || ""}</td>
             </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
 }
 
-export default ResultTable
+export default ResultTable;
