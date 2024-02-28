663a11b60dd2889168eb2ce8a6226764d3bd2fbe
Himanshu <hv65616@gmail.com>
2024-02-28 16:15:27 +0530

    get server data and post server data function created

--- Quiz_App/client/src/helper/helper.js
+++ Quiz_App/client/src/helper/helper.js
@@ -1,5 +1,6 @@
-import {useSelector} from "react-redux"
-import {Navigate} from "react-router-dom"
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 export function attemptsNumber(result) {
   return result.filter((r) => r !== undefined).length;
 }
@@ -16,7 +17,17 @@ export function checkresult(totalPoints, earnPoints) {
   return (totalPoints * 50) / 100 < earnPoints;
 }
 
-export function CheckUserExists({children}){
+export function CheckUserExists({ children }) {
   const auth = useSelector((state) => state.result.userId);
   return auth ? children : <Navigate to={"/"} replace={true}></Navigate>;
+}
+
+export async function getServerData(url, callback) {
+  const data = await (await axios.get(url))?.data;
+  return callback ? callback(data) : data;
+}
+
+export async function postServerData(url, result, callback) {
+  const data = await (await axios.get(url, result))?.data;
+  return callback ? callback(data) : data;
 }
