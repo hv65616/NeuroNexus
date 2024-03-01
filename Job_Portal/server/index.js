731d7449df335c6c48a9b91b6c806d9945d7448f
Himanshu <hv65616@gmail.com>
2024-03-01 21:48:27 +0530

    db import routes import error handler import

--- Job_Portal/server/index.js
+++ Job_Portal/server/index.js
@@ -3,9 +3,14 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const fileupload = require("express-fileupload");
-
+const userRouter = require("./routes/userRoutes");
+const applicationRouter = require("./routes/applicationRoutes");
+const jobRouter = require("./routes/jobRoutes");
+const dbConnection = require("./database/dbConnection");
+const { errorMiddleware } = require("./middlewares/error");
 const app = express();
 dotenv.config({ path: "./config.env" });
+dbConnection();
 
 app.use(
   cors({
@@ -19,11 +24,17 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
 app.use(
   fileupload({
     useTempFiles: true,
     tempFileDir: "/temp/",
   })
 );
+
+app.use(errorMiddleware)
 
 module.exports = app;
