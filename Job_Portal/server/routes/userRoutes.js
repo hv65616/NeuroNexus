8086edff463991406392120d584bc64e75239393
Himanshu <hv65616@gmail.com>
2024-03-06 15:58:31 +0530

    logout route

--- Job_Portal/server/routes/userRoutes.js
+++ Job_Portal/server/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cookieparser = require("cookie-parser");
 const userController = require("../controllers/userController");
+const isauth = require("../middlewares/auth");
 const router = express.Router();
+router.use(cookieparser());
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.get("/logout", userController.logout);
 module.exports = router;
