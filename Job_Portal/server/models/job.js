364f28c302946944f07cabfaab3a1612914228bc
Himanshu <hv65616@gmail.com>
2024-03-08 17:19:19 +0530

    job model created

---
+++ Job_Portal/server/models/job.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please provide job title"],
+    minLength: [3, "Job title must contain atleast 3 characters"],
+    maxLength: [50, "Job title cannot exceed  30 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide job description"],
+    minLength: [3, "Job description must contain atleast 3 characters"],
+    maxLength: [50, "Job description cannot exceed  30 characters"],
+  },
+  category: {
+    type: String,
+    required: [true, "Job category is required"],
+  },
+  city: {
+    type: String,
+    required: [true, "Job city is required"],
+  },
+  location: {
+    type: String,
+    required: [true, "Please provide exact location"],
+    minLength: [50, "Job location must contain at least 50 characters"],
+  },
+  fixedSalary: {
+    type: Number,
+    minLength: [4, "Fixed salary must contain atleast 4 digits"],
+    maxLength: [9, "Fixed salary connot exceed 9 digits"],
+  },
+  salaryFrom: {
+    type: Number,
+    minLength: [4, "Salary From must contain atleast 4 digits"],
+    maxLength: [9, "Salary From cannot exceed 9 digits"],
+  },
+  salaryTo: {
+    type: Number,
+    minLength: [4, "Salary To must contain at least 4 digits"],
+    maxLength: [9, "Salary To cannot exceed 9 digits"],
+  },
+  expired: {
+    type: String,
+    default: false,
+  },
+  jobPostedOn: {
+    type: Date,
+    default: Date.now(),
+  },
+  postedBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+module.exports = Job;
