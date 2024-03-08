42d71c027db619deabc993c9e8eed173525783a6
Himanshu <hv65616@gmail.com>
2024-03-08 17:20:46 +0530

    generating jwt with id

--- Job_Portal/server/utils/jwtToken.js
+++ Job_Portal/server/utils/jwtToken.js
@@ -1,11 +1,16 @@
+const jwt = require("jsonwebtoken");
 const sendToken = (user, statusCode, res, message) => {
-  const token = user.getJWTToken();
+  const payload = { id: user._id };
+  // const token = user.getJWTToken();
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true,
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
