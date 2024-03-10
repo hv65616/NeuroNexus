2c6e1ab9402634c6d831b175a8e19fd36b9d5a87
Himanshu <hv65616@gmail.com>
2024-03-10 20:55:14 +0530

    get my job and update job route created

--- Job_Portal/server/routes/jobRoutes.js
+++ Job_Portal/server/routes/jobRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { model } = require("mongoose");
 const jobController = require("../controllers/jobController");
-const isAuth = require("../middlewares/auth")
+const isAuth = require("../middlewares/auth");
 const router = express.Router();
 router.get("/getall", jobController.getAllJobs);
 router.post("/post",isAuth,jobController.postJob)
+router.get("/getmyjobs", isAuth,jobController.getMyJobs);
+router.put("/update/:id", isAuth, jobController.updateJob);
 module.exports = router;
