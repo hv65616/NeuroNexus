a37c8670ceca1f8fbed363d3683fe54223f7d36e
Himanshu <hv65616@gmail.com>
2024-03-10 20:54:53 +0530

    get my job and update job controller created

--- Job_Portal/server/controllers/jobController.js
+++ Job_Portal/server/controllers/jobController.js
@@ -11,7 +11,7 @@ const getAllJobs = catchAsync(async (req, res, next) => {
 });
 const postJob = catchAsync(async (req, res, next) => {
   const { role } = req.user;
-//   console.log(role);
+  //   console.log(role);
   if (role === "Job Seeker") {
     return next(
       new ErrorHandler("Job Seeker is not allowed to access this resouce", 400)
@@ -63,7 +63,45 @@ const postJob = catchAsync(async (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: "Job Posted successfully",
-    job
+    job,
   });
 });
-module.exports = { getAllJobs, postJob };
+
+const getMyJobs = catchAsync(async (req, res, next) => {
+  const role = req.user;
+  if (role === "Job Seeker") {
+    return next(
+      new ErrorHandler("Job Seeker is not allowed to access this resouce", 400)
+    );
+  }
+  const myjobs = await Job.find({ postedBy: req.user._id });
+  res.status(200).json({
+    success: true,
+    myjobs,
+  });
+});
+
+const updateJob = catchAsync(async (req, res, next) => {
+  const role = req.user;
+  if (role === "Job Seeker") {
+    return next(
+      new ErrorHandler("Job Seeker is not allowed to access this resouce", 400)
+    );
+  }
+  const { id } = req.params;
+  let job = await Job.findById(id);
+  if (!job) {
+    return next(new ErrorHandler("Oops, job not found!", 404));
+  }
+  job = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    job,
+    msg: "Job Updated Successfully",
+  });
+});
+module.exports = { getAllJobs, postJob, getMyJobs, updateJob };
