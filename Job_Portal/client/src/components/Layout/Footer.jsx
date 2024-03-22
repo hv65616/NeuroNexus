3f07ec0150cdb785121f102752d26ee2c76761ed
Himanshu <hv65616@gmail.com>
2024-03-22 19:15:32 +0530

    footer component designed

--- Job_Portal/client/src/components/Layout/Footer.jsx
+++ Job_Portal/client/src/components/Layout/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 const Footer = () => {
+  const { isAuthorized } = useContext(Context);
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className={isAuthorized ? "footerShow" : "footerHide"}></footer>
+      <div>&copy; All Right Reserver By Himanshu Verma</div>
+      <div>
+        <Link to={"/"} target="_blank">
+          <FaFacebookF></FaFacebookF>
+        </Link>
+        <Link to={"/"} target="_blank">
+          <FaLinkedin></FaLinkedin>
+        </Link>
+        <Link to={"/"} target="_blank">
+          <RiInstagramFill></RiInstagramFill>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
