389ed7ff0e3d3506f4fa4dc23a081960cb5f9607
Himanshu <hv65616@gmail.com>
2024-03-23 19:12:40 +0530

    register page completed

--- Job_Portal/client/src/components/Auth/Register.jsx
+++ Job_Portal/client/src/components/Auth/Register.jsx
@@ -1,9 +1,135 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { Context } from "../../main";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { FaPencilAlt, FaRegUser } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaPhoneFlip } from "react-icons/fa6";
+import { RiLock2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [phone, setPhone] = useState("");
 
-export default Register
+  const { isAuthorized, setIsAuthorized, user, setUser } = useContext(Context);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/user/register",
+        { name, email, password, role, phone },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast.success(data.message);
+      setName("");
+      setEmail("");
+      setPhone("");
+      setRole("");
+      setPassword("");
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+  if (isAuthorized) {
+    return navigate("/");
+  }
+  return (
+    <>
+      <div className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/JobZeelogo.png" alt="Logo" />
+            <h3>Create New Account</h3>
+          </div>
+          <form action="">
+            <div className="inputTag">
+              <label htmlFor="">Register As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Select Role</option>
+                  <option value="Employer">Employer</option>
+                  <option value="Job Seeker">Job Seeker</option>
+                </select>
+                <FaRegUser></FaRegUser>
+              </div>
+            </div>
+
+            <div className="inputTag">
+              <label htmlFor="">Name</label>
+              <div>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Enter your name"
+                />
+                <FaPencilAlt></FaPencilAlt>
+              </div>
+            </div>
+
+            <div className="inputTag">
+              <label htmlFor="">Email</label>
+              <div>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                />
+                <MdOutlineMailOutline></MdOutlineMailOutline>
+              </div>
+            </div>
+
+            <div className="inputTag">
+              <label htmlFor="">Phone Number</label>
+              <div>
+                <input
+                  type="number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  placeholder="Enter your phone number"
+                />
+                <FaPhoneFlip></FaPhoneFlip>
+              </div>
+            </div>
+
+            <div className="inputTag">
+              <label htmlFor="">Password</label>
+              <div>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your password"
+                />
+                <RiLock2Fill></RiLock2Fill>
+              </div>
+            </div>
+
+            <button onClick={handleRegister} type="submit">
+              Register
+            </button>
+            <Link to={"/login"}>Login Now</Link>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/register.png" alt="Register" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
