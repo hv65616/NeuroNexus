51b662f31c0f6ce84dbcbe4a1c84f278b4dce395
Himanshu <hv65616@gmail.com>
2024-03-24 15:36:18 +0530

    not used dependencies remove

--- Job_Portal/client/src/App.jsx
+++ Job_Portal/client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 import "./App.css";
 import { Context } from "./main";
 import Login from "./components/Auth/Login";
