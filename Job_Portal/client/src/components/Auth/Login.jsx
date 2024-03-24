a5d59a044b715fbfc3bbe6f9566b3ad0af56e1d9
Himanshu <hv65616@gmail.com>
2024-03-24 15:39:34 +0530

    login component created

--- Job_Portal/client/src/components/Auth/Login.jsx
+++ Job_Portal/client/src/components/Auth/Login.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { Context } from "../../main";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import {Navigate} from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLock2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
 
-export default Login
+  const { isAuthorized, setIsAuthorized, user, setUser } = useContext(Context);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/user/login",
+        { email, password, role },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast.success(data.message);
+      setEmail("");
+      setRole("");
+      setPassword("");
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+  if (isAuthorized) {
+    return <Navigate to={"/"}></Navigate>;
+  }
+  return (
+    <>
+      <div className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/JobZeelogo.png" alt="Logo" />
+            <h3>Login To Your Account</h3>
+          </div>
+          <form action="">
+            <div className="inputTag">
+              <label htmlFor="">Login As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Select Role</option>
+                  <option value="Employer">Employer</option>
+                  <option value="Job Seeker">Job Seeker</option>
+                </select>
+                <FaRegUser></FaRegUser>
+              </div>
+            </div>
+
+            <div className="inputTag">
+              <label htmlFor="">Email</label>
+              <div>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                />
+                <MdOutlineMailOutline></MdOutlineMailOutline>
+              </div>
+            </div>
+
+            <div className="inputTag">
+              <label htmlFor="">Password</label>
+              <div>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your password"
+                />
+                <RiLock2Fill></RiLock2Fill>
+              </div>
+            </div>
+
+            <button onClick={handleLogin} type="submit">
+              Login
+            </button>
+            <Link to={"/register"}>Register Now</Link>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/login.png" alt="Login" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
