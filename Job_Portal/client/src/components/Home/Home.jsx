3cc8c3dec6ff14db06907ea5dc2cdf20300e3364
Himanshu <hv65616@gmail.com>
2024-03-25 22:31:04 +0530

    hoge page design

--- Job_Portal/client/src/components/Home/Home.jsx
+++ Job_Portal/client/src/components/Home/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import HowItWorks from "./HowItWorks";
+import PopularCategories from "./PopularCategories";
+import PopularCompanies from "./PopularCompanies";
 const Home = () => {
+  const { isAuthorized } = useContext(Context);
+  if (!isAuthorized) {
+    return <Navigate to={"/login"}></Navigate>;
+  }
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="homePage page">
+        <HeroSection></HeroSection>
+        <HowItWorks></HowItWorks>
+        <PopularCategories></PopularCategories>
+        <PopularCompanies></PopularCompanies>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- Job_Portal/client/src/components/Home/HowItWorks.jsx
+++ Job_Portal/client/src/components/Home/HowItWorks.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <>
+    </>
   )
 }
 
