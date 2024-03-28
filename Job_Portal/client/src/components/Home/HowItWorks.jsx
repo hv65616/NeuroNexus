e637177262d737a0b98fbe2eec89bc4f719ad820
Himanshu <hv65616@gmail.com>
2024-03-28 23:20:40 +0530

    how it works page designed

--- Job_Portal/client/src/components/Home/HowItWorks.jsx
+++ Job_Portal/client/src/components/Home/HowItWorks.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import {MdFindInPage} from "react-icons/md"
+import {IoMdSend} from "react-icons/io"
 const HowItWorks = () => {
   return (
     <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>How JobZee Workd</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus></FaUserPlus>
+              <p>Create Account</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis harum soluta doloribus ex itaque et!
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage></MdFindInPage>
+              <p>Find a Job/Post a Job</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis harum soluta doloribus ex itaque et!
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend></IoMdSend>
+              <p>Create Account</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis harum soluta doloribus ex itaque et!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HowItWorks
+export default HowItWorks;
