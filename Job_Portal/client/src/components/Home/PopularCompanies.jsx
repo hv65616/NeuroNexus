fd7af2ead81955bd0452a4f3b8addb810e6385e4
Himanshu <hv65616@gmail.com>
2024-03-29 23:56:52 +0530

    just to maintain streak

--- Job_Portal/client/src/components/Home/PopularCompanies.jsx
+++ Job_Portal/client/src/components/Home/PopularCompanies.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const PopularCompanies = () => {
-  return (
-    <div>PopularCompanies</div>
-  )
-}
+  return <></>;
+};
 
-export default PopularCompanies
+export default PopularCompanies;
