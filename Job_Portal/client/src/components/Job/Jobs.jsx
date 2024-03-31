578fe9f2605a28c1c25323d35b4e07175777b0f9
Himanshu <hv65616@gmail.com>
2024-03-31 23:57:19 +0530

    building job page

--- Job_Portal/client/src/components/Job/Jobs.jsx
+++ Job_Portal/client/src/components/Job/Jobs.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../main";
+import { useNavigate, Link } from "react-router-dom";
+import Axios from "axios";
 const Jobs = () => {
-  return (
-    <div>Jobs</div>
-  )
-}
+  const [jobs, setJobs] = useState([]);
+  const { isAuthorized } = useContext(Context);
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    try {
+      Axios.get("http://localhost:4000/api/v1/job/getall", {
+        withCredentials: true,
+      }).then((res) => {
+        setJobs(res.data.jobs);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  if (!isAuthorized) {
+    navigateTo("/login");
+  }
 
-export default Jobs
+  return (
+    <>
+      <section className="jobs page">
+        <div className="container">
+          <h1>All Available Jobs</h1>
+          <div className="banner">
+            {jobs.jobs &&
+              jobs.jobs.map((ele) => {
+                return (
+                  <div className="card" key={ele._id}>
+                    <p>{ele.title}</p>
+                    <p>{ele.category}</p>
+                    <p>{ele.country}</p>
+                    <Link to={`/job/${ele._id}`}>Job Details</Link>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Jobs;
